0521839172824cf60fe744be832d1c0e8f5d3454
Daniel Ritchie <dritchie1031@gmail.com>
2025-09-19 16:03:36 -0700

    react version

---
+++ _next/static/chunks/4faf42cfd3cf5c12.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/71e2bbae79425380.js",
+  "static/chunks/turbopack-ae01f10e129b140c.js"
+])
---
+++ _next/static/chunks/ce7642daff7510c6.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/71e2bbae79425380.js",
+  "static/chunks/turbopack-b983ca703151fe16.js"
+])
